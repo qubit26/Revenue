9d8e033e7830985a53061a16c85b9069a4fdb12b
qubit26 <58095357+qubit26@users.noreply.github.com>
2023-06-05 12:59:33 -0400

    Creando el modulo de busqueda

--- management/views.py
+++ management/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def busqueda(request):
+    if request.method == 'GET':    
+        return render(request, 'busqueda.html')
